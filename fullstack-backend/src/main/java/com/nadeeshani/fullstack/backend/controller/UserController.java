4c203d892be82663a749a8e61f7a4df9b20e9977
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-26 15:19:13 +0530

    added the functionality to edit the user

--- fullstack-backend/src/main/java/com/nadeeshani/fullstack/backend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/nadeeshani/fullstack/backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.nadeeshani.fullstack.backend.controller;
 
+import com.nadeeshani.fullstack.backend.exception.UserNotFoundException;
 import com.nadeeshani.fullstack.backend.model.User;
 import com.nadeeshani.fullstack.backend.repository.UserRepository;
 import org.apache.catalina.LifecycleState;
@@ -24,4 +25,28 @@ public class UserController {
     List<User> getAllUsers(){
         return userRepository.findAll();
     }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    user.setCountry(newUser.getCountry());
+                    user.setAge(newUser.getAge());
+                    user.setGender(newUser.getGender());
+                    user.setProfileImageUrl(newUser.getProfileImageUrl());
+                    user.setMessage(newUser.getMessage());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+
 }
