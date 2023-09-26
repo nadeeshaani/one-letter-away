7541f56db600ff5122574b294ec6d724403e5956
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-26 15:48:56 +0530

    updated frontend to delete user using axios DELETE

--- fullstack-frontend/src/pages/Home.js
+++ fullstack-frontend/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function Home() {
     const [users,setUsers]=useState([])
+
+    const {id}=useParams()
 
     useEffect(()=>{
         loadUsers();
@@ -13,6 +15,11 @@ export default function Home() {
         const result = await axios.get("http://localhost:8080/users")
         setUsers(result.data)
         console.log(result.data)
+    }
+
+    const deleteUser=async (id)=>{
+      await axios.delete(`http://localhost:8080/user/${id}`)
+      loadUsers()
     }
 
   return (
@@ -49,7 +56,7 @@ export default function Home() {
         <td className='d-flex align-items-center'>
             <button className="btn btn-dark mx-2">View</button>
             <Link className="btn btn-outline-dark mx-2" to={`/edituser/${user.id}`}>Edit</Link>
-            <button className="btn btn-outline-danger mx-2">Delete</button>
+            <button className="btn btn-outline-danger mx-2" onClick={()=>deleteUser(user.id)}>Delete</button>
         </td>
         
 
