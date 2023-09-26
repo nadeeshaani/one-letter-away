edd2501cb4d80d0c480244190b70bfb9a165d755
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-26 08:48:11 +0530

    stored user state information

--- fullstack-frontend/src/users/AddUser.js
+++ fullstack-frontend/src/users/AddUser.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AddUser() {
+
+    const [user,setUser]=useState({
+        name:"",
+        username:"",
+        age:0,
+        gender:"",
+        message:"",
+        country:"",
+        profileImage:null
+
+    })
+
+    const{name,username,email,age,gender,message,country,profileImage}=user
+
+    const onInputChange=(e)=>{
+        setUser({...user,[e.target.name]:e.target.value})
+    }
+
   return (
     <div className='container'>
         <div className="row">
@@ -13,7 +31,9 @@ export default function AddUser() {
                     <input type={"text"}
                     className='form-control'
                     placeholder='Enter your name'
-                    name='name'/>
+                    name='name'
+                    value={name}
+                    onChange={(e)=>onInputChange(e)}/>
                 </div>
 
                 <div className='mb-3'>
@@ -23,7 +43,9 @@ export default function AddUser() {
                     <input type={"text"}
                     className='form-control'
                     placeholder='Enter your username'
-                    name='username'/>
+                    name='username'
+                    value={username}
+                    onChange={(e)=>onInputChange(e)}/>
                 </div>
 
                 <div className='mb-3'>
@@ -33,14 +55,16 @@ export default function AddUser() {
                     <input type={"text"}
                     className='form-control'
                     placeholder='Enter your email address'
-                    name='email'/>
+                    name='email'
+                    value={email}
+                    onChange={(e)=>onInputChange(e)}/>
                 </div>
 
                 <div className='mb-3'>
   <label htmlFor='profileImage' className='form-label'>
     Profile Image
   </label>
-  <input type="file" className='form-control' name='profileImage' accept='image/*' />
+  <input type="file" className='form-control' name='profileImage' accept='image/*' value={profileImage} onChange={(e)=>onInputChange(e)}/>
 </div>
 
 
@@ -48,10 +72,12 @@ export default function AddUser() {
                     <label htmlFor='Age' className='form-label'>
                         Age
                     </label>
-                    <input type={"number"}
+                    <input type={"age"}
                     className='form-control'
                     placeholder='Enter your age'
-                    name='age'/>
+                    name='age'
+                    value={age}
+                    onChange={(e)=>onInputChange(e)}/>
                 </div>
 
                 <div className='mb-3'>
@@ -61,14 +87,28 @@ export default function AddUser() {
                     <input type={"text"}
                     className='form-control'
                     placeholder='Enter your country'
-                    name='country'/>       
+                    name='country'
+                    value={country}
+                    onChange={(e)=>onInputChange(e)}/>       
                 </div>
 
                 <div className='mb-3'>
-                    <label htmlFor='Country' className='form-label mx-2'>
+                    <label htmlFor='Message' className='form-label'>
+                        Message
+                    </label>
+                    <input type={"text"}
+                    className='form-control'
+                    placeholder='Enter your message'
+                    name='message'
+                    value={message}
+                    onChange={(e)=>onInputChange(e)}/>
+                </div>
+
+                <div className='mb-3'>
+                    <label htmlFor='Gender' className='form-label mx-2'>
                         Gender
                     </label>
-                    <select id="gender" name="gender">
+                    <select id="gender" name="gender" value={gender} onChange={(e)=>onInputChange(e)}>
   <option value="Male">Male</option>
   <option value="Female">Female</option>
   <option value="Other">Other</option>
